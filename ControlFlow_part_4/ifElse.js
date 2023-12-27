c230e79dcb9ae105a2218b194582a370bca41ef9
satyam-chauhan-21 <archauhan4390@gmail.com>
2023-12-27 18:52:15 +0530

    adding Control Flow files

---
+++ ControlFlow_part_4/ifElse.js
@@ -0,0 +1,24 @@
+// in this file I've learn about Control Flow means IF-Else.
+
+/**
+ * I already know about IF-Else statments so don't want to write any discription about that.
+ */
+
+//------------------------------------------------------------//
+//      Program to check positive or negative number.
+//------------------------------------------------------------//
+
+// const num1 = ( 4 + 4 );
+// const num1 = ( 4 + 9 );
+const num1 = ( 4 - 4 );
+
+if (num1 > 0) {
+    console.log(`${num1} is Positive.`)
+}
+else if (num1 < 0) {
+    console.log(`${num1} is Negative.`)
+}
+else {
+    console.log(`${num1} is Zero`)
+}
+
---
+++ ControlFlow_part_4/swtich.js
@@ -0,0 +1,27 @@
+// in this file I've learn about Switch statement.
+
+// const month = 2;
+// const month = 4;
+const month = 6;
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+
+    default:
+        console.log(`Month ${month} does not match.`);
+        break;
+}
