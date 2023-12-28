d7eee8b4cde2ff7518ad1d762a7e54fc183a7c92
satyam-chauhan-21 <archauhan4390@gmail.com>
2023-12-28 20:44:43 +0530

    add Iteration repo

---
+++ Iteration_part_5/loops.js
@@ -0,0 +1,12 @@
+// In this file I've learn about Loops in JS.
+/**
+ * - for loop
+ * - while loop
+ * - do while loop
+ * - 
+ */
+
+/**
+ * I know about For loop, While loop & Do While loop so i don't write anything about that.
+ */
+
