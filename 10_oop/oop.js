93259094c4209b04901175acb750155135e379be
satyam-chauhan-21 <archauhan4390@gmail.com>
2025-01-19 22:38:22 +0530

    object constructor ke baare me aur knowledge

--- 10_oop/oop.js
+++ 10_oop/oop.js
@@ -4,6 +4,7 @@
 // object literal
 // object constructor & "this" keyword
 // Problem & new keyword
+// "instaceof" operator ke baare me padh lena.
 
 //-------------------------------------------------------------------------------------------------------//
 //                                              JS type
@@ -37,17 +38,18 @@
 // object constructor ek function hota hai jise ham object banane ke liye use karte hai.
 // object constructor banane ka ek sahi rule hai ki function ka name ka first letter capital ho.
 // for example:
-function Person(fname, lname, age, salary){
-    // ab yaha par ham ese likhe de
-    // fname = fname // to fir yaha confusion ho jayegi ki parameter me jo varibale hai vo konsa hai & object ka variable konsa hai.
-    // to uske liye hame "this" ka use karna hoga object ka variable alag se batane ke liye & parameter vale variable ko ese hi normally likh denge.
-    this.fname = fname 
-    this.lname = lname 
-    this.age = age 
-    this.salary = salary 
 
-    return this
-}
+// function Person(fname, lname, age, salary){
+//     // ab yaha par ham ese likhe de
+//     // fname = fname // to fir yaha confusion ho jayegi ki parameter me jo varibale hai vo konsa hai & object ka variable konsa hai.
+//     // to uske liye hame "this" ka use karna hoga object ka variable alag se batane ke liye & parameter vale variable ko ese hi normally likh denge.
+//     this.fname = fname 
+//     this.lname = lname 
+//     this.age = age 
+//     this.salary = salary 
+
+//     return this
+// }
 
 // yaha tak hamne "this" dekh liya , use bhi kar liya
 
@@ -55,11 +57,36 @@ function Person(fname, lname, age, salary){
 
 // const empOne = Person("karan", "singh", 21, 21000000)
 // console.log(empOne); // dekho yaha ek problem aayi, 
-// // hame object costructor banane ke baad usme se object ko return bhi karna padta hai. so abhi uper vale object constructor me "return this" likhna padega.
+// // hame object costructor banane ke baad usme se object ko return bhi karna padta hai. so abhi uper vale object constructor me "return this" likhna padega. 
+// dekho ye abhi pata chal raha hai ki "return this" likhna koi jaruri nahi hai bina likhe bhi kaam ho jata hai.
+// ye check karne ke liye ek object constructor bana kar dekhte hai.
+function User(userName, pswd, isLoggedIn){
+    this.userName = userName
+    this.pswd = pswd
+    this.isLoggedIn = isLoggedIn
+}
+const firstUser = User("kara", '123', true)
+console.log(firstUser) // output: undefined, 
+// ye output milne ke 2 reason hai,
+// first to hamne "new" keyword ka use nahi kiya hai. and 
+// second ke if ham "new" ka use na kar ke "return this" ka use kare to hame faltu ke chije bhi output me milegi but problem solve ho jayegi.
+
+
+// const secondUser = new User("kara", '123', true)
+// console.log(secondUser) // output: undefined
 
 //-------------------------------------------------------------------------------------------------------//
 //                                            Problem
 //-------------------------------------------------------------------------------------------------------//
+
+// function Person(fname, lname, age, salary){
+//     this.fname = fname 
+//     this.lname = lname 
+//     this.age = age 
+//     this.salary = salary 
+
+//     return this
+// }
 
 // // ab main problem dekhte hai.
 // // hamne uper object constructor ka ek instance banaya now ab dusra instance banate hai
@@ -70,8 +97,8 @@ function Person(fname, lname, age, salary){
 // // so iss overwrite ki problem se bachne ke liye instance creation ke time "new" keyword ka use hota hai. joki memory me instances ko new memory asign karta hai so overwrite ki problem nahi hoti hai.
 
 // const empOne = new Person("karan", "singh", 21, 21000000)
-const empOne = Person("karan", "singh", 21, 21000000)
+// const empOne = Person("karan", "singh", 21, 21000000)
 // const empTwo = new Person("manav", "singh", 21, 21000000)
 // console.log(empTwo);// yaha par new keyword nahi likhte hai na to fir output me bohot sari chije show karta hai jo kaam ki bhi nahi hai is time par.
-console.log(empOne);
+// console.log(empOne);
 
